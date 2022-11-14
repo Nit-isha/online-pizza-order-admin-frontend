85c5a5119461a28cc28c07c9d27f5d52aa282d3a
Harit Yadav <harityadavdk@gmail.com>
2022-11-14 13:49:51 +0530

    add design to addPizza

--- src/components/pizza/AddPizza.jsx
+++ src/components/pizza/AddPizza.jsx
@@ -41,25 +41,61 @@ export default function AddPizza() {
                             .catch(err => setError(err.message));
                     }}
                 >
-                    <label htmlFor="pizzaType">Pizza Type</label>
-                        <input type="text" name="pizzaType" id="pizzaType"  required autoFocus />
-                        <br />
-                        <label htmlFor="pizzaName">Pizza Name</label>
-                        <input type="text" name="pizzaName" id="pizzaName" required />
-                        <br />
-                        <label htmlFor="pizzaSize">Pizza Size</label>
-                        <input type="text" name="pizzaSize" id="pizzaSize"  required />
-                        <br />
-                        <label htmlFor="pizzaDesc">Pizza Description</label>
-                        <textarea rows={3} type="text" name="pizzaDesc" id="pizzaDesc"  required />
-                        <br />
-                        <label htmlFor="pizzaCost">Pizza Cost</label>
-                        <input type="number" name="pizzaCost" id="pizzaCost"  required />
-                        <br />
-                        <input type="submit" className='btn btn-success mx-2 my-1' value="Add" />
-                        <input type="button" className='btn btn-default mx-2 my-1' onClick={() => navigate("/menu")} value="Cancel" />
-                        <p id="error">{error}</p>
+                    {/* <label htmlFor="pizzaType">Pizza Type</label>
+                    <input type="text" name="pizzaType" id="pizzaType" required autoFocus />
+                    <br />
+                    <label htmlFor="pizzaName">Pizza Name</label>
+                    <input type="text" name="pizzaName" id="pizzaName" required />
+                    <br />
+                    <label htmlFor="pizzaSize">Pizza Size</label>
+                    <input type="text" name="pizzaSize" id="pizzaSize" required />
+                    <br />
+                    <label htmlFor="pizzaDesc">Pizza Description</label>
+                    <textarea rows={3} type="text" name="pizzaDesc" id="pizzaDesc" required />
+                    <br />
+                    <label htmlFor="pizzaCost">Pizza Cost</label>
+                    <input type="number" name="pizzaCost" id="pizzaCost" required />
+                    <br />
+                    <input type="submit" className='btn btn-success mx-2 my-1' value="Add" />
+                    <input type="button" className='btn btn-default mx-2 my-1' onClick={() => navigate("/menu")} value="Cancel" />
+                    <p id="error">{error}</p> */}
+
+
+                    <div className="form-group row ">
+                        <label htmlFor="pizzaType" className="col-sm-2 col-form-label">Pizza type</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control-plaintext" style={{border:"solid 1px black"}}name="pizzaType" id="pizzaType" placeholder="Enter pizza type" required/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label" htmlFor="pizzaName">Pizza Name</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control-plaintext" style={{border:"solid 1px black"}} name="pizzaName" id="pizzaName" required />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label" htmlFor="pizzaSize">Pizza Size</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control-plaintext" style={{border:"solid 1px black"}}  name="pizzaSize" id="pizzaSize"  required/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label" htmlFor="pizzaDesc">Pizza Description</label>
+                        <div className="col-sm-10">
+                            <textarea rows={3} className="form-control-plaintext" style={{border:"solid 1px black"}} type="text" name="pizzaDesc" id="pizzaDesc" required />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label" htmlFor="pizzaCost">Pizza Cost</label>
+                        <div className="col-sm-10">
+                            <input type="number" className="form-control-plaintext" style={{border:"solid 1px black"}} name="pizzaCost" id="pizzaCost" required/>
+                        </div>
+                    </div>
+                    <input type="submit" className='btn btn-success mx-2 my-1' value="Add" />
+                    <input type="button" className='btn btn-default mx-2 my-1' onClick={() => navigate("/menu")} value="Cancel" />
+                    <p id="error">{error}</p>
                 </form>
+
             }
         </div>
     )
